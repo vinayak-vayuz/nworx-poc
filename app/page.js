bc31f9e8fd66b1fbb7c2526814802ba141ff4dfd
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2024-02-09 12:23:22 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ const buttons = [
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
+    <main className={`${inter.className} flex min-h-screen`}>
       <div className="w-full md:w-[15%] min-h-screen bg-[#F58A43] flex flex-col items-center">
         <Image
           src={"/images/nworx_logo.svg"}
@@ -27,14 +27,31 @@ export default function Home() {
         />
         <div className="flex flex-col justify-center min-h-[80%]">
           {buttons.map((data, index) => (
-            <button key={index} className={`${data.title === "Act on Goals" ? "bg-white" : ""} group hover:bg-white p-2 rounded flex gap-2 mb-3`}>
-              <Image className="hover:text-[#F58A43] group-hover:stroke-[#F58A43]" src={data.icon} width={24} height={25} alt="icons" />
-              <span className={`${inter.className} font-semibold text-white group-hover:text-[#F58A43]`}>{data.label}</span>
+            <button
+              key={index}
+              className={`${
+                data.label === "Act on Goals" ? "bg-white" : ""
+              } group hover:bg-white p-2 rounded flex gap-2 mb-3`}
+            >
+              <Image
+                className="hover:text-[#F58A43] group-hover:stroke-[#F58A43]"
+                src={data.icon}
+                width={24}
+                height={25}
+                alt="icons"
+              />
+              <span
+                className={`${
+                  data.label === "Act on Goals" ? "text-[#F58A43]" : ""
+                } font-semibold text-white group-hover:text-[#F58A43]`}
+              >
+                {data.label}
+              </span>
             </button>
           ))}
         </div>
       </div>
-      <div className="w-full md:w-[85%]"></div>
+      <div className="w-full md:w-[85%] bg-[#F8F8F8]"></div>
     </main>
   );
 }
